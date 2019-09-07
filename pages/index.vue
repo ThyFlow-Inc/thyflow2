22bcb067a74f3a791f105aa2c240ff48cdcdf869
Yusuf Adeyemo <Yusadolat@gmail.com>
2019-09-07 22:16:01 +0100

    Add features :zap:

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <div>
     <Header />
     <HowItWorks />
+    <Features />
   </div>
 </template>
 
 <script>
 import Header from "~/components/Header.vue";
 import HowItWorks from "~/components/HowItWorks.vue";
+import Features from "~/components/Features.vue";
 export default {
   components: {
     Header,
-    HowItWorks
+    HowItWorks,
+    Features
   }
 };
 </script>
