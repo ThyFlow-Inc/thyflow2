6b7164e2705d840c848718371ea9581006f44e59
Yusuf Adeyemo <Yusadolat@gmail.com>
2019-09-21 15:04:11 +0100

    Added COntact Page :zap:

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
+    <header class="pt-9 gradient">
+      <div class="container px-1 py-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+        <div
+          class="flex flex-col w-full md:w-5/5 justify-center items-center content-center md:text-center"
+        >
+          <h1 class="my-6 text-5xl font-bold leading-tight text-center text-blue-600">THYFLOW</h1>
+          <h3 class="my-4 text-4xl font-bold leading-tight text-center">CONTACT US</h3>
+        </div>
+      </div>
+    </header>
+    <div class="container mx-auto px-8 lg:px-48">
+      <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">CONTACT US</h1>
+
+      <div class="pb-16">
+        <div class="mb-4 text-2xl font-bold">ThyFlow</div>
+        <p class="font-bold">Eastwood Cir</p>
+        <p class="font-bold">Santa Clara, California 95054</p>
+        <p class="font-bold">
+          Email Address:
+          Sals@Thyflow.com
+        </p>
+        <p class="font-bold">
+          Telephone number:
+          3106500196
+        </p>
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
@@ -13,5 +40,8 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
+.gradient {
+  background: linear-gradient(90deg, #0a3ead 0%, #e38914 100%);
+}
 </style>
