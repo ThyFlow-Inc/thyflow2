0423138ff057c3a5692b1108b79087629eaeec9a
Yusuf Adeyemo <Yusadolat@gmail.com>
2019-09-22 14:55:37 +0100

    Remove redundat page

--- components/Footer.vue
+++ components/Footer.vue
@@ -91,12 +91,6 @@
                 class="no-underline hover:underline text-gray-800 hover:text-orange-500"
               >Suggest City</nuxt-link>
             </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <nuxt-link
-                to="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >#</nuxt-link>
-            </li>
           </ul>
         </div>
       </div>
