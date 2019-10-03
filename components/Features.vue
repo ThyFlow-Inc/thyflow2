ec706d379b792fbd5a29bb55fa11475ddeb46abd
Yusadolat <yusadolat@gmail.com>
2019-10-03 15:46:46 +0100

    Change Teat color :fire:

--- components/Features.vue
+++ components/Features.vue
@@ -11,16 +11,16 @@
 
       <div class="flex flex-wrap">
         <div class="w-5/6 sm:w-1/2 p-6">
-          <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">Book the best</h3>
-          <p class="text-gray-600 mb-6 font-bold">
+          <h3 class="text-3xl text-black-800 font-bold leading-none mb-3">Book the best</h3>
+          <p class="text-black-600 mb-6 font-bold">
             ThyFlow barbers have years of experiences.
             <br />
           </p>
-          <p class="text-gray-600 mb-6 font-bold">
+          <p class="text-black-600 mb-6 font-bold">
             ThyFlow Master Barber verify every new barber upon joining the platform.
             <br />
           </p>
-          <p class="text-gray-600 mb-6 font-bold">
+          <p class="text-black-600 mb-6 font-bold">
             World class customer support.
             <br />
           </p>
@@ -36,17 +36,17 @@
         </div>
         <div class="w-full sm:w-1/2 p-6 mt-6">
           <div class="align-middle">
-            <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">Promise to customers</h3>
-            <p class="text-gray-600 mb-5 font-bold">
-              Like your haircut or your money back
+            <h3 class="text-3xl text-black-800 font-bold leading-none mb-3">Promise to customers</h3>
+            <p class="text-black-600 mb-5 font-bold">
+              Like your haircut or your money back.
               <br />
             </p>
-            <p class="text-gray-600 mb-5 font-bold">
-              Experience barbershop from the comfort of your home
+            <p class="text-black-600 mb-5 font-bold">
+              Experience barbershop from the comfort of your home.
               <br />
             </p>
-            <p class="text-gray-600 mb-5 font-bold">
-              Kids get free haircut with any adult
+            <p class="text-black-600 mb-5 font-bold">
+              Kids get free haircut with any adult.
               <br />
             </p>
           </div>
@@ -55,20 +55,20 @@
 
       <div class="flex flex-wrap">
         <div class="w-5/6 sm:w-1/2 p-6">
-          <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">ThyFLow Way</h3>
-          <p class="text-gray-600 mb-6 font-bold">
+          <h3 class="text-3xl text-black-800 font-bold leading-none mb-3">ThyFLow Way</h3>
+          <p class="text-black-600 mb-6 font-bold">
             Barbershop experience at the comfort of your house.
             <br />
           </p>
-          <p class="text-gray-600 mb-6 font-bold">
+          <p class="text-black-600 mb-6 font-bold">
             Licensed Barbers.
             <br />
           </p>
-          <p class="text-gray-600 mb-6 font-bold">
+          <p class="text-black-600 mb-6 font-bold">
             Two-step interview process.
             <br />
           </p>
-          <p class="text-gray-600 mb-6 font-bold">
+          <p class="text-black-600 mb-6 font-bold">
             Friends and Family referral confirmed
             <br />
           </p>
