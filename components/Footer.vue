dfbfd532c7a158eb9e4ed7bece203520c42669d9
Yusadolat <yusadolat@gmail.com>
2019-10-06 15:56:55 +0100

    Added Privacy Page :zap:

--- components/Footer.vue
+++ components/Footer.vue
@@ -46,7 +46,7 @@
             </li>
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <nuxt-link
-                to="#"
+                to="/privacy"
                 class="no-underline hover:underline text-gray-800 hover:text-orange-500"
               >Privacy</nuxt-link>
             </li>
