d634849376624975d47933f1bc4989654927b027
Yusadolat <yusadolat@gmail.com>
2019-10-15 16:46:13 +0100

    Added Navbar to suggest a city

--- pages/suggest.vue
+++ pages/suggest.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
+    <Navbar />
     <Footer />
   </div>
 </template>
 
 <script>
+import Navbar from "~/components/Navbar.vue";
 import Footer from "~/components/Footer.vue";
 export default {
   components: {
+    Navbar,
     Footer
   }
 };
