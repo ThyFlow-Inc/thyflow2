26243c50ba7bd19ee189cc32aca7b443a82493c8
Yusadolat <yusadolat@gmail.com>
2019-10-25 08:33:07 +0100

    Added Blog Page :zap:

---
+++ pages/blog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue';
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
