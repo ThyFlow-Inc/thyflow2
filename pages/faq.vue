d20078bd7774a80ed91d45a87379358368abaa48
Yusadolat <yusadolat@gmail.com>
2019-11-26 12:48:26 +0100

    Pardin Bottom 8

--- pages/faq.vue
+++ pages/faq.vue
@@ -27,7 +27,7 @@
         <div class="mb-4 text-2xl font-bold">How experienced are the barbers?</div>
         <p>ThyFlow Barbers have a minimum of 3+ years of experience cutting hair, Before joining the platform.</p>
       </div>
-      <div class="pb-16">
+      <div class="pb-8">
         <div class="mb-4 text-2xl font-bold">How far in advance can I book an appointment?</div>
         <p>You can book an appointment 2 hours in advance.</p>
       </div>
@@ -37,22 +37,22 @@
         <p>It varies from one hour to an Hour and a half. Depending on the haircut you choose.</p>
       </div>
 
-      <div class="pb-16">
+      <div class="pb-8">
         <div class="mb-4 text-2xl font-bold">Can I guarantee I will get the same barber each time?</div>
         <p>
           Depending on the area you live, Most of our barbers service their neighborhood and local areas, But if you rate a barber below 3 stars, you will not get
           the same barber again.
         </p>
       </div>
-      <div class="pb-16">
+      <div class="pb-8">
         <div class="mb-4 text-2xl font-bold">How will quality be guaranteed?</div>
         <p>All of our Barbers are professionals that take pride in their work. They take the time to ensure the quality and standard is met. Barbers are handpicked by industry professionals.</p>
       </div>
-      <div class="pb-16">
+      <div class="pb-8">
         <div class="mb-4 text-2xl font-bold">Do I need to provide anything?</div>
         <p>Yes, An area for the haircut to take place, Like your backyard, garage, or an empty room, You can use your office chair. Barbers will have own chair if you don't have a good standard chair.</p>
       </div>
-      <div class="pb-16">
+      <div class="pb-8">
         <div class="mb-4 text-2xl font-bold">Do you do Kids hair cuts?</div>
         <p>Yes, All kids under 10 years old, get free haircut with any adult haircut.</p>
       </div>
