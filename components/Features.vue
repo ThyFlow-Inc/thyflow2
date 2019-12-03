1af7cf76ed82d09eb76b9a4f95b1623b1f65bb4b
Yusadolat <yusadolat@gmail.com>
2019-12-03 12:57:59 +0100

    Changing Color, Orange look :zap:

--- components/Features.vue
+++ components/Features.vue
@@ -12,12 +12,12 @@
         <div class="w-5/6 sm:w-1/2 p-6">
           <h3 class="text-3xl text-black-800 font-bold leading-none mb-3">Book the best</h3>
           <ul class="list-disc px-4">
-          <li class="text-black-600 mb-5">ThyFlow barbers have years of experience.</li>
-          <li class="text-black-600 mb-5">ThyFlow Barber provides exceptional customer service.</li>
-          <li
-            class="text-black-600 mb-5"
-          >ThyFlow Master Barber verify every new barber upon joining the platform.</li>
-          <li class="text-black-600 mb-5">World-class customer support.</li>
+            <li class="text-orange-800 mb-5">ThyFlow barbers have years of experience.</li>
+            <li class="text-orange-800 mb-5">ThyFlow Barber provides exceptional customer service.</li>
+            <li
+              class="text-orange-800 mb-5"
+            >ThyFlow Master Barber verify every new barber upon joining the platform.</li>
+            <li class="text-orange-800 mb-5">World-class customer support.</li>
           </ul>
         </div>
         <div class="w-full sm:w-1/2 p-6">
@@ -33,9 +33,9 @@
           <div class="align-middle">
             <h3 class="text-3xl text-black-800 font-bold leading-none mb-3">Promise to customers</h3>
             <ul class="list-disc px-4">
-            <li class="tex-black-600 mb-5">Expect barbershop experience from your home.</li>
-            <li class="text-black-600 mb-5">Like your haircut or your money back.</li>
-            <li class="text-black-600 mb-5">Kids get a free haircut with any adult haircut.</li>
+              <li class="text-orange-800 mb-5">Expect barbershop experience from your home.</li>
+              <li class="text-orange-800 mb-5">Like your haircut or your money back.</li>
+              <li class="text-orange-800 mb-5">Kids get a free haircut with any adult haircut.</li>
             </ul>
           </div>
         </div>
@@ -45,10 +45,10 @@
         <div class="w-5/6 sm:w-1/2 p-6">
           <h3 class="text-3xl text-black-800 font-bold leading-none mb-3">Vetted Barbers</h3>
           <ul class="list-disc px-4">
-          <li class="text-black-600 mb-5">Barbers Background checked.</li>
-          <li class="text-black-600 mb-5">Licensed Barbers.</li>
-          <li class="text-black-600 mb-5">Two-step interview process.</li>
-          <li class="text-black-600 mb-5">Friends and Family referral.</li>
+            <li class="text-orange-800 mb-5">Barbers Background checked.</li>
+            <li class="text-orange-800 mb-5">Licensed Barbers.</li>
+            <li class="text-orange-800 mb-5">Two-step interview process.</li>
+            <li class="text-orange-800 mb-5">Friends and Family referral.</li>
           </ul>
         </div>
         <div class="w-full sm:w-1/2 p-6">
