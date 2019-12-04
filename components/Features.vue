1076decb66b0617d0d5e1e3cf5d9a2a1a418a07e
Yusadolat <yusadolat@gmail.com>
2019-12-04 10:34:25 +0100

    Chnages for Features :zap:

--- components/Features.vue
+++ components/Features.vue
@@ -3,21 +3,21 @@
   <section class="bg-white border-b py-8">
     <div class="container max-w-7xl mx-auto m-8">
       <h1
-        class="w-full my-2 text-5xl font-bold leading-tight text-center text-orange-500"
+        class="w-full my-2 text-5xl font-bold leading-tight text-center text-black-600"
       >Book with confidence</h1>
       <div class="w-full mb-4">
         <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
       </div>
       <div class="flex flex-wrap">
         <div class="w-5/6 sm:w-1/2 p-6">
-          <h3 class="text-3xl text-black-800 font-bold leading-none mb-3">Book the best</h3>
+          <h3 class="text-3xl text-orange-500 font-bold leading-none mb-3">Book the best</h3>
           <ul class="list-disc px-4">
-            <li class="text-orange-800 mb-5">ThyFlow barbers have years of experience.</li>
-            <li class="text-orange-800 mb-5">ThyFlow Barber provides exceptional customer service.</li>
+            <li class="text-black-800 mb-5">ThyFlow barbers have years of experience.</li>
+            <li class="text-black-800 mb-5">ThyFlow Barber provides exceptional customer service.</li>
             <li
-              class="text-orange-800 mb-5"
+              class="text-black-800 mb-5"
             >ThyFlow Master Barber verify every new barber upon joining the platform.</li>
-            <li class="text-orange-800 mb-5">World-class customer support.</li>
+            <li class="text-black-800 mb-5">World-class customer support.</li>
           </ul>
         </div>
         <div class="w-full sm:w-1/2 p-6">
@@ -31,11 +31,11 @@
         </div>
         <div class="w-full sm:w-1/2 p-6 mt-6">
           <div class="align-middle">
-            <h3 class="text-3xl text-black-800 font-bold leading-none mb-3">Promise to customers</h3>
+            <h3 class="text-3xl text-orange-500 font-bold leading-none mb-3">Promise to customers</h3>
             <ul class="list-disc px-4">
-              <li class="text-orange-800 mb-5">Expect barbershop experience from your home.</li>
-              <li class="text-orange-800 mb-5">Like your haircut or your money back.</li>
-              <li class="text-orange-800 mb-5">Kids get a free haircut with any adult haircut.</li>
+              <li class="text-black-800 mb-5">Expect barbershop experience from your home.</li>
+              <li class="text-black-800 mb-5">Like your haircut or your money back.</li>
+              <li class="text-black-800 mb-5">Kids get a free haircut with any adult haircut.</li>
             </ul>
           </div>
         </div>
@@ -43,12 +43,12 @@
 
       <div class="flex flex-wrap">
         <div class="w-5/6 sm:w-1/2 p-6">
-          <h3 class="text-3xl text-black-800 font-bold leading-none mb-3">Vetted Barbers</h3>
+          <h3 class="text-3xl text-orange-500 font-bold leading-none mb-3">Vetted Barbers</h3>
           <ul class="list-disc px-4">
-            <li class="text-orange-800 mb-5">Barbers Background checked.</li>
-            <li class="text-orange-800 mb-5">Licensed Barbers.</li>
-            <li class="text-orange-800 mb-5">Two-step interview process.</li>
-            <li class="text-orange-800 mb-5">Friends and Family referral.</li>
+            <li class="text-black-800 mb-5">Barbers Background checked.</li>
+            <li class="text-black-800 mb-5">Licensed Barbers.</li>
+            <li class="text-black-800 mb-5">Two-step interview process.</li>
+            <li class="text-black-800 mb-5">Friends and Family referral.</li>
           </ul>
         </div>
         <div class="w-full sm:w-1/2 p-6">
