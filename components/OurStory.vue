5f30fd7e11ee22ecef19050c127b25f7f632ef25
Yusadolat <yusadolat@gmail.com>
2019-12-10 17:40:01 +0100

    Bigger Text

--- components/OurStory.vue
+++ components/OurStory.vue
@@ -5,7 +5,7 @@
         <p class="w-full my-2 text-2xl font-bold text-center text-gray-800">Our Story</p>
         <div class="w-full mb-4">
           <p
-            class="w-full my-2 text-1xl text-center text-black-600 px-16 sm:align-middle md:align-middle"
+            class="w-full my-2 lg:text-2xl text-center text-black-600 px-16 sm:align-middle md:align-middle"
           >
             At ThyFlow, We believe a great looking haircut gives you confident to achieve your goals. Having confidence in yourself will help you do anything you want in this life and be a leader among your friends. ThyFlow Mobile Barbers help you be more confident by providing a world class haircut service at the comfort of your home or wherever.
             ThyFlow service is convenient and reliable for anyone with a busy lifestyle. Only the top local barbers on the platform!
