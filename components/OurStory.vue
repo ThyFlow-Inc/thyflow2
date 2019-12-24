0b614a58cc1d16832dce8e9c3d7f80c6c46664c1
Yusadolat <yusadolat@gmail.com>
2019-12-24 19:09:36 +0100

    Change Background Color :zap:

--- components/OurStory.vue
+++ components/OurStory.vue
@@ -2,14 +2,15 @@
   <div>
     <section class="bg-white border-b py-24 lg:px-64 px-8 story-section mt-8">
       <a class="container mx-auto flex flex-wrap pt-4 rounded bg-white">
-        <p class="w-full my-2 text-3xl font-bold text-center text-gray-800">Our Story</p>
+        <p class="w-full my-2 text-3xl lg:text-5xl font-bold text-center text-gray-800">Our Story</p>
         <div class="w-full mb-4">
           <p
             class="w-full my-2 lg:text-2xl text-center text-black-600 px-16 sm:align-middle md:align-middle"
           >
             At
             <span class="text-orange-500 inline font-bold">ThyFlow</span>, We believe a great looking haircut gives you confident to achieve your goals. Having confidence in yourself will help you do anything you want in this life and be a leader among your friends. ThyFlow Mobile Barbers help you be more confident by providing a world class haircut service at the comfort of your home or wherever.
-            ThyFlow service is convenient and reliable for anyone with a busy lifestyle. Only the top local barbers on platform!
+            ThyFlow service is convenient and reliable for anyone with a busy lifestyle.
+            <span>Only the top local barbers on platform!</span>
           </p>
         </div>
       </a>
@@ -23,7 +24,7 @@ export default {};
 
 <style lang="css" scoped>
 .story-section {
-  background-image: url("../assets/hero.png");
+  background-color: orange;
   background-size: cover;
 }
 </style>
