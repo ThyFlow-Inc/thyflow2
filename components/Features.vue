8bb60ab0743d7da3d4612fe3844e178844568326
Yusadolat <yusadolat@gmail.com>
2020-02-15 18:19:46 +0100

    Changes with ease :zap:

--- components/Features.vue
+++ components/Features.vue
@@ -4,7 +4,7 @@
     <div class="container max-w-7xl mx-auto m-8">
       <h1
         class="my-3 md:text-6xl uppercase sm:text-3xl font-bold leading-tight text-center text-white"
-      >Book with confidence</h1>
+      >Book with ease</h1>
       <div class="w-full mb-4">
         <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
       </div>
