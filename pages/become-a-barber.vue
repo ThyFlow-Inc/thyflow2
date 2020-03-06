a584d901da94aa93def7a8a88eebb475b563de16
Yusadolat <yusadolat@gmail.com>
2020-03-06 09:52:59 +0100

    Revsmping the barber page :zap:

--- pages/become-a-barber.vue
+++ pages/become-a-barber.vue
@@ -1,23 +1,80 @@
 <template>
   <div>
     <Navbar />
-    <div class="container mx-auto px-4">
-      <div class="flex flex-wrap -mx-4 my-5">
+    <div class="header">
+      <div class="flex flex-wrap mx-4">
         <!--Left Col-->
-        <div class="w-full lg:w-2/5 px-4 text-center lg:text-left self-center">
-          <h1 class="mb-12 text-5xl font-bold leading-none text-orange">Become a ThyFlow Barber</h1>
+        <div class="w-full lg:w-3/6 px-4 text-center lg:text-left self-center ">
+          <h1 class="mx-12 text-5xl font-bold leading-none text-orange">Become a ThyFlow Barber</h1>
           <p class="mb-12">
-            <a
-              class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8"
-              href="#"
-            ></a>
-          </p>
+            <p
+              class="mx-16 text-white font-semibold lg:text-1xl"
+            
+            > If you are looking for a flexible way to earn a great income doing what you enjoy, Join ThyFlow Barber Team. 
+            THYFLOW - This is an alternative way for men to get a haircut at home. ThyFlow, offer customers Barbershop experience at home or wherever they at. 
+            ThyFlow Barbers work within their neighborhood to drive to customer's homes to provide a haircut service.</p>
+          
           <a class="mx-auto lg:mx-0 hover:underline text-gray-800 font-bold my-6 py-4 px-8"></a>
         </div>
 
         <!--Right Col-->
-        <div class="w-full lg:w-3/5 px-4 text-center lg:text-right lg:text-left self-center px-4">
-          <img src="~/assets/barber.png" class="w-full xl:w-4/5 z-50 inline-block" />
+        <div class="w-full lg:w-3/6 px-4 text-center lg:center lg:text-left self-center px-12">
+    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
+  <div class="-mx-3 md:flex mb-6">
+    <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
+        First Name
+      </label>
+      <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Jane">
+      <p class="text-red text-xs italic">Please fill out this field.</p>
+    </div>
+    <div class="md:w-1/2 px-3">
+      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-last-name">
+        Last Name
+      </label>
+      <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-last-name" type="text" placeholder="Doe">
+    </div>
+  </div>
+  <div class="-mx-3 md:flex mb-6">
+    <div class="md:w-full px-3">
+      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-password">
+        Password
+      </label>
+      <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3" id="grid-password" type="password" placeholder="******************">
+      <p class="text-grey-dark text-xs italic">Make it as long and as crazy as you'd like</p>
+    </div>
+  </div>
+  <div class="-mx-3 md:flex mb-2">
+    <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-city">
+        City
+      </label>
+      <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-city" type="text" placeholder="Albuquerque">
+    </div>
+    <div class="md:w-1/2 px-3">
+      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-state">
+        State
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded" id="grid-state">
+          <option>New Mexico</option>
+          <option>Missouri</option>
+          <option>Texas</option>
+        </select>
+        <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
+          <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
+      </div>
+    </div>
+    <div class="md:w-1/2 px-3">
+      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-zip">
+        Zip
+      </label>
+      <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-zip" type="text" placeholder="90210">
+    </div>
+  </div>
+</div>
+          <!---<img src="~/assets/barber.png" class="w-full xl:w-4/5 z-50 inline-block" />-->
         </div>
       </div>
     </div>
@@ -25,7 +82,7 @@
     <div class="container mx-auto lg:px-24">
       <div class="pb-16">
         <div class="mb-4 text-2xl font-bold text-center">Become a Mobile Barber</div>
-        <p class="text-center">
+        <p class="text-center text-2xl">
           If you are looking for a flexible way to earn a great income doing what you enjoy, Join ThyFlow Barber Team. THYFLOW - This is an alternative way for men to get a haircut at home. ThyFlow, offer customers Barbershop experience at home or wherever they at.
           ThyFlow Barbers work within their neighborhood to drive to customer's homes to provide a haircut service.
         </p>
@@ -111,5 +168,17 @@ export default {
 <style lang="css" scoped>
 .gradient {
   background: linear-gradient(90deg, #0a3ead 0%, #e38914 100%);
+}
+.header {
+  height: 80vh;
+  background-image: linear-gradient(
+      to right,
+      rgba(88, 201, 236, 0.801),
+      rgba(28, 58, 228, 0.603)
+    ),
+    url("~assets/barber_22.jpg");
+  background-size: cover;
+  background-position: top;
+  position: relative;
 }
 </style>
