5fe4f95e3d97e7dc0bea866cc49ca28e969673e1
Yusadolat <Yusadolat@gmail.com>
2020-03-25 19:12:06 +0100

    Change URl for View baber profile

--- components/ApplyBarber.vue
+++ components/ApplyBarber.vue
@@ -45,7 +45,7 @@
 <button
           class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
         >
-          <a href="https://thyflow.co/listing">View Barbers Profile</a>
+          <a href="https://zfrmz.com/YwaCWiBx7wEJ3E7hNmZl">View Barbers Profile</a>
         </button>
       
     </section>
