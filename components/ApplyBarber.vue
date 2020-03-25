7ea138fcdce6e441963110d9b2d1c89f34d6a122
Yusadolat <Yusadolat@gmail.com>
2020-03-25 14:51:25 +0100

    Change Book a babrber to View babrbers profile

--- components/ApplyBarber.vue
+++ components/ApplyBarber.vue
@@ -45,7 +45,7 @@
 <button
           class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
         >
-          <a href="#">Book a barber!</a>
+          <a href="https://thyflow.co/listing">View Barbers Profile</a>
         </button>
       
     </section>
