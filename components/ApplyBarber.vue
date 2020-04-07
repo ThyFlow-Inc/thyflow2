c930ce615fcbde7a6bb0f43ac9b58f31cefc399d
Yusadolat <Yusadolat@gmail.com>
2020-04-07 18:10:04 +0100

    💡 Redesign the Apply barber :zap:

--- components/ApplyBarber.vue
+++ components/ApplyBarber.vue
@@ -1,61 +1,26 @@
 <template>
-  <div class="gradient">
-    <svg
-      class="wave-top"
-      viewBox="0 0 1439 147"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-    >
-      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g transform="translate(-1.000000, -14.000000)" fill-rule="nonzero">
-          <g class="wave" fill="#f8fafc">
-            <path
-              d="M1440,84 C1383.555,64.3 1342.555,51.3 1317,45 C1259.5,30.824 1206.707,25.526 1169,22 C1129.711,18.326 1044.426,18.475 980,22 C954.25,23.409 922.25,26.742 884,32 C845.122,37.787 818.455,42.121 804,45 C776.833,50.41 728.136,61.77 713,65 C660.023,76.309 621.544,87.729 584,94 C517.525,105.104 484.525,106.438 429,108 C379.49,106.484 342.823,104.484 319,102 C278.571,97.783 231.737,88.736 205,84 C154.629,75.076 86.296,57.743 0,32 L0,0 L1440,0 L1440,84 Z"
-            />
-          </g>
-          <g transform="translate(1.000000, 15.000000)" fill="#FFFFFF">
-            <g
-              transform="translate(719.500000, 68.500000) rotate(-180.000000) translate(-719.500000, -68.500000) "
-            >
-              <path
-                d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496"
-                opacity="0.100000001"
-              />
-              <path
-                d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
-                opacity="0.100000001"
-              />
-              <path
-                d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z"
-                opacity="0.200000003"
-              />
-            </g>
-          </g>
-        </g>
-      </g>
-    </svg>
-    <section class=" flex items-center flex-col mx-auto mover-animate">
-
-
-      <div class="lg:w-2/4 lg:pl-4">
-
+  <div class="mt-6">
+ 
+    <section class=" flex items-center flex-col mx-auto">
+      <div class="md:flex bg-white rounded-lg p-6 lg:m-16">
+      <div class="flex flex-wrap items-center justify-between -mx-4">
+        <div class="my-4 px-6 lg:w-auto">
       <h6
-        class="w-full md:text-4xl sm:text-1xl  lg:px-2 font-bold leading-tight text-center text-white"
-      >Imagine having your own personal barber when you want with a tap of a finger</h6>
+        class="mb-3 text-white font-normal text-3xl text-orange-500"
+      >Imagine having your own personal barber, <br> <strong><span class="text-2xl">When you want with a tap of a finger</span></strong></h6>
+        </div>
+        <div class="w-full px-4 md:w-auto lg:text-center">
+          <a href="#" class="ml-4 rounded-lg px-4 md:px-5 xl:px-4 py-4 md:py-4 xl:py-3 bg-transparent hover:bg-white rounded border border-orange-500 md:text-lg xl:text-xl text-orange-700 font-semibold leading-tight">Book a Barber</a>
+          <a class="bg-orange-500 rounded-lg ml-4 px-4 md:px-5 xl:px-4 py-4 md:py-4 xl:py-3 hover:bg-gray-200 md:text-lg xl:text-xl text-white font-semibold leading-tight shadow-md" href="https://zfrmz.com/YwaCWiBx7wEJ3E7hNmZl">View Barbers Profile</a>
+        </div>
       </div>
       
+      </div>
     </section>
     <!------footer button------>
-    <section class="container mx-auto text-center py-8 px-24 mover-animate">
-    <button
-          class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
-        >
-          <a href="https://zfrmz.com/YwaCWiBx7wEJ3E7hNmZl">View Barbers Profile</a>
-        </button>
-
-    </section>
   </div>
+
+  
 </template>
 
 <script>
