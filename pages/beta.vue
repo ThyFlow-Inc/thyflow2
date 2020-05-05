e41d3b45df1c3d14780927a2dee0f27be9df55d6
Yusadolat <Yusadolat@gmail.com>
2020-05-05 11:09:54 +0100

    Added Prefecch to Beta page :zap:

--- pages/beta.vue
+++ pages/beta.vue
@@ -100,6 +100,7 @@ export default {
         },
         {
           rel: "stylesheet",
+          rel: "preload",
           href:
             "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
         },
