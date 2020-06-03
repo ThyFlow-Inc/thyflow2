0b1a115570ae273761559f59efa540adaf2926a8
Yusadolat <Yusadolat@gmail.com>
2020-06-03 19:51:07 +0100

    Send my Invite

--- components/BetaForm1.vue
+++ components/BetaForm1.vue
@@ -108,7 +108,7 @@
           <button
             class="uppercase bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-lg"
           >
-            Send me an Invite
+            Send my Invite
           </button>
         </div>
       </form>
