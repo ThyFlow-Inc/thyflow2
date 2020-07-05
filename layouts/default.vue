3b2d1f688c7753d263cf09eec0201582dcecab3d
Yusuf Adeyemo <yusadolat@gmail.com>
2020-07-05 23:32:13 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="$nuxt.isOffline">Ops! You are offline</div>
+    <div v-if="$nuxt.isOffline">Ops! You are offline Please check your Internet Connection</div>
     <nuxt keep-alive />
   </div>
 </template>
