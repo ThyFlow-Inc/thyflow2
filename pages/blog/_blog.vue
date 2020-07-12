9dac17b3654b5f70e9e646664ed0573ccc9fe5fc
Yusadolat <Yusadolat@gmail.com>
2020-07-12 22:53:25 +0100

    Remove barber nave blog :zap:

--- pages/blog/_blog.vue
+++ pages/blog/_blog.vue
@@ -13,13 +13,7 @@
         </a>
         <nav
           class="md:ml-auto flex flex-wrap items-center text-base justify-center"
-        >
-          <nuxt-link
-            to="/barber"
-            class="mr-5 text-orange-500 text-lg font-bold hover:text-gray-900"
-            >Join ThyFlow as a Barber</nuxt-link
-          >
-        </nav>
+        ></nav>
         <button
           class="inline-flex text-white font-bold items-center bg-orange-500 py-1 px-3  hover:bg-orange-300 rounded text-lg mt-4 md:mt-0"
         >
